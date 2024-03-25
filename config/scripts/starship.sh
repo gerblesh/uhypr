c297d3c3a87be7a0750752a500e8f74aa22fbdfa
gerblesh <101901964+gerblesh@users.noreply.github.com>
2024-03-25 10:39:43 -0700

    fix: add devel packages + starship

---
+++ config/scripts/starship.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+curl -Lo /tmp/starship.tar.gz "https://github.com/starship/starship/releases/latest/download/starship-x86_64-unknown-linux-gnu.tar.gz"
+tar -xzf /tmp/starship.tar.gz -C /tmp
+install -c -m 0755 /tmp/starship /usr/bin
+echo 'if status is-interactive; starship init fish | source; end' >> /etc/fish/config.fish
