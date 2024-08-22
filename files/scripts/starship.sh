7ec60302018c658632eb741bcc79dd4513dffa82
gerblesh <101901964+gerblesh@users.noreply.github.com>
2024-08-21 17:53:26 -0700

    unshit the bed

--- files/scripts/starship.sh
+++ files/scripts/starship.sh
@@ -3,4 +3,4 @@
 curl -Lo /tmp/starship.tar.gz "https://github.com/starship/starship/releases/latest/download/starship-x86_64-unknown-linux-gnu.tar.gz"
 tar -xzf /tmp/starship.tar.gz -C /tmp
 install -c -m 0755 /tmp/starship /usr/bin
-echo 'if status is-interactive; starship init fish | source; end' >> /etc/fish/config.fish
+#echo 'if status is-interactive; starship init fish | source; end' >> /etc/fish/config.fish
