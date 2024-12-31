a658efb09edd6028c20d0d9d69554598449cc2fb
gerblesh <101901964+gerblesh@users.noreply.github.com>
2024-12-30 21:50:17 -0800

    update scripts

--- files/usr/bin/screenshot.sh
+++ files/usr/bin/screenshot.sh
@@ -5,5 +5,4 @@ FILE="$(grimshot save $1)"
 
 if [[ -f "$FILE" ]]; then
     cat $FILE | wl-copy
-    notify-send "Screenshot" "Screenshot taken, copied to clipboard and saved in Pictures folder" --icon "$FILE" --app-name "screenshot.sh"
 fi
--- files/usr/bin/wallpaper-menu.sh
+++ files/usr/bin/wallpaper-menu.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env fish
 
-#wezterm start --class "floatterm" --cwd ~/Pictures/Wallpapers/Mocha/ fish -ic 'wallpaper (pwd)/(fzf)'
-foot -w 1000x1000 --app-id "floatterm" -D ~/Pictures/Wallpapers/Mocha/ fish -ic 'wallpaper (pwd)/(fzf)'
+# wezterm start --always-new-process --class "floatterm" --cwd ~/Pictures/Wallpapers/Mocha/ fish -ic 'wallpaper (pwd)/(fzf)'
+
+ghostty --title="floatterm" --working-directory="~/Pictures/Wallpapers/Mocha" -e "/bin/fish '-ic cd ~/Pictures/Wallpapers/Mocha && yazi --chooser-file $HOME/.config/wallpaper'"
 
 wallpaper init
