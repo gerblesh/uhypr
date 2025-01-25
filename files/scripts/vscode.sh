4defc26cf0fde672c06eeb88c3acbd714987c3ef
gerblesh <101901964+gerblesh@users.noreply.github.com>
2025-01-24 23:28:19 -0800

    update

--- files/scripts/vscode.sh
+++ files/scripts/vscode.sh
@@ -8,14 +8,14 @@ set -oue pipefail
 # docs: https://code.visualstudio.com/docs/setup/linux#_rhel-fedora-and-centos-based-distributions
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
 
-# echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | tee /etc/yum.repos.d/vscode.repo > /dev/null
+# echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://paceages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | tee /etc/yum.repos.d/vscode.repo > /dev/null
 
 cat << EOF > /etc/yum.repos.d/vscode.repo
 [code]
-  name=Visual Studio Code
-  baseurl=https://packages.microsoft.com/yumrepos/vscode
-  enabled=1
-  gpgcheck=1
-  gpgkey=https://packages.microsoft.com/keys/microsoft.asc
+name=Visual Studio Code
+baseurl=https://packages.microsoft.com/yumrepos/vscode
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.microsoft.com/keys/microsoft.asc
 EOF
 # vscode is installed in the packages module, this script simply installs the repo
